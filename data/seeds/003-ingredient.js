2588571eb97d91ba9d65bdb8ebd72793095cc49a
Sara <scearc930@gmail.com>
2020-10-09 10:03:58 -0400

    wrote out routes and data shows to postman

--- data/seeds/003-ingredient.js
+++ data/seeds/003-ingredient.js
@@ -2,13 +2,34 @@
 exports.seed = function(knex) {
  const ingredient = [
    {
-     ingredient_name: "water"
+     ingredient_name: "water" // id: 1
    },
    {
-    ingredient_name: "beans"
+    ingredient_name: "beans" // id: 2
   },
   {
-    ingredient_name: "salt"
+    ingredient_name: "salt" // id: 3
+  },
+  {
+    ingredient_name: "flat iron steak" // id: 4
+  },
+  {
+    ingredient_name: "seasonings" // id: 5
+  },
+  {
+    ingredient_name: "cream soda" // id: 6
+  },
+  {
+    ingredient_name: "carmel sauce" // id: 7
+  },
+  {
+    ingredient_name: "whipped cream" // id: 8
+  },
+  {
+    ingredient_name: "skewers" // id: 9
+  },
+  {
+    ingredient_name: "air fryer" // id: 10
   }
  ]
  return knex("ingredient").insert(ingredient)
